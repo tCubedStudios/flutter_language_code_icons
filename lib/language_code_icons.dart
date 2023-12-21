378e1a3895018342915c7c18b1d7e6571486c140
Chris Sprague <spragucm@gmail.com>
2023-12-21 13:47:18 -0800

    Added EN and ES icons

--- lib/language_code_icons.dart
+++ lib/language_code_icons.dart
@@ -1,7 +1,12 @@
+// ignore_for_file: non_constant_identifier_names, constant_identifier_names
 library language_code_icons;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class LanguageCodeIcons {
+  static const String FONT_FAMILY = "LanguageCodes";
+  static const IconData EN = IconData(0xe838, fontFamily: FONT_FAMILY);
+  static const IconData EN_SOLID = IconData(0xe938, fontFamily: FONT_FAMILY);
+  static const IconData ES = IconData(0xe840, fontFamily: FONT_FAMILY);
+  static const IconData ES_SOLID = IconData(0xe940, fontFamily: FONT_FAMILY);
 }
